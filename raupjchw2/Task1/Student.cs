548c1a287f7ea90866420f1c6f40ef9bc3e6ff59
Ivan Maltar <ivan.maltar@fer.hr>
2017-10-29 21:20:54 +0100

    task 1

--- raupjchw2/Task1/Student.cs
+++ raupjchw2/Task1/Student.cs
@@ -2,7 +2,46 @@
 
 namespace Task1
 {
-    public class Student
+    public class Student: Object
     {
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; set; }
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if(obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Student student = (Student)obj;
+            return (Name == student.Name && Jmbag == student.Jmbag && Gender == student.Gender);
+        }
+
+        public override int GetHashCode()
+        {
+            return Jmbag.GetHashCode() * Name.GetHashCode() * Gender.GetHashCode(); 
+        }
+
+        public static bool operator ==(Student s1, Student s2)
+        {
+            return s1.Equals(s2);
+        }
+
+        public static bool operator !=(Student s1, Student s2)
+        {
+            return !s1.Equals(s2);
+        }
+    }
+
+    public enum Gender
+    {
+        Male, Female
     }
 }
