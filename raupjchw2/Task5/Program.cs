6b9c61425e72e8d7261a4fe8a6872464a74bab3b
Ivan Maltar <ivan.maltar@fer.hr>
2017-11-01 15:20:07 +0100

    Task 5

---
+++ raupjchw2/Task5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            LongOperation("A");
+            LongOperation("B");
+            LongOperation("C");
+            LongOperation("D");
+            LongOperation("E");
+            stopwatch.Stop();
+            Console.WriteLine(" Synchronous long operation calls finished {0} sec.",
+            stopwatch.Elapsed.TotalSeconds);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            Parallel.Invoke(() => LongOperation("A"),
+            () => LongOperation("B"),
+            () => LongOperation("C"),
+            () => LongOperation("D"),
+            () => LongOperation("E"));
+            stopwatch.Stop();
+            Console.WriteLine(" Parallel long operation calls finished {0} sec.",
+            stopwatch.Elapsed.TotalSeconds);
+
+            int counter = 0;
+            Parallel.For(0, 100000, (i) =>
+            {
+                Thread.Sleep(1);
+                counter += 1;
+            });
+            Console.WriteLine(" Counter should be 100000. Counter is {0}", counter);
+
+            counter = 0;
+            object objectUsedForLock = new object();
+            Parallel.For(0, 100000, (i) =>
+            {
+                Thread.Sleep(1);
+                lock (objectUsedForLock)
+                {
+                    counter += 1;
+                }
+            });
+            Console.WriteLine(" Counter should be 100000. Counter is {0}", counter);
+
+            List<int> results = new List<int>();
+            Parallel.For(0, 100000, (i) =>
+            {
+                Thread.Sleep(1);
+                results.Add(i * i);
+            });
+            Console.WriteLine("Bag length should be 100000. Length is {0}", results.Count);
+
+            ConcurrentBag<int> iterations = new ConcurrentBag<int>();
+            Parallel.For(0, 100000, (i) =>
+            {
+                Thread.Sleep(1);
+                iterations.Add(i);
+            });
+            Console.WriteLine("Bag length should be 100000. Length is {0}",
+            iterations.Count);
+
+            //////////////////
+            Console.ReadLine();
+        }
+
+        public static void LongOperation(string taskName)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} Finished . Executing Thread : {1}", taskName,
+            Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ raupjchw2/Task5/readme.md
@@ -0,0 +1,10 @@
+﻿#Pitanje 1:
+Izvođenje programa je trajalo 5,0122521 sec.
+#Pitanje 2:
+Sve operacije su se izvodine na jednoj dretvi.
+#Pitanje 3:
+Izvođenje programa trajalo je 1,0515608 sec.
+#Pitanje 4:
+Svaka operacija se izvodila na nekoj drugoj dretvi (ukupno 5 dretvi).
+#Pitanje 5:
+Uovom primjeru istovremeni pristup izvršavanja koraka može rezultirati neželjeno ponašanje jer ne znamo koji korak će se prvi izvesti. Na primjer problem može nastati ako krenemo uvećati vrijednost koju još nismo dohvatili ili spremimo novu vrijednost bez da smo je završili željenu operaciju nad njom.
