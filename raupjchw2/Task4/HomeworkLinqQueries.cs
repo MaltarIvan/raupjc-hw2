0d8c18b9c3b25d1532773b451862dfd6f4af09d6
Ivan Maltar <ivan.maltar@fer.hr>
2017-11-01 14:26:19 +0100

    Task 4 incompleted

---
+++ raupjchw2/Task4/HomeworkLinqQueries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Task1;
+
+namespace Task4
+{
+    public class HomeworkLinqQueries
+    {
+        public static string[] Linq1(int[] intArray)
+        {
+            throw new NotImplementedException();
+        }
+        public static University[] Linq2_1(University[] universityArray)
+        {
+            return universityArray.Where(u => u.Students.All(s => s.Gender == Gender.Male)).ToArray();
+        }
+        public static University[] Linq2_2(University[] universityArray)
+        {
+            return universityArray.Where(u => u.Students.Count() < universityArray.Average(s => s.Students.Count())).ToArray();
+        }
+        public static Student[] Linq2_3(University[] universityArray)
+        {
+            return universityArray.SelectMany(u => u.Students).Distinct().Cast<Student>().ToArray();
+        }
+        public static Student[] Linq2_4(University[] universityArray)
+        {
+            return universityArray.Where(u => u.Students.All(s => s.Gender == Gender.Male) || u.Students.All(s => s.Gender == Gender.Female)).SelectMany(s => s.Students).Distinct().Cast<Student>().ToArray(); ;
+        }
+        public static Student[] Linq2_5(University[] universityArray)
+        {
+            return universityArray.SelectMany(u => u.Students).GroupBy(s => s.Jmbag).Where(s => s.Count() >= 2).Cast<Student>().ToArray();
+        }
+    }
+}
---
+++ raupjchw2/Task4/University.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task1;
+
+namespace Task4
+{
+    public class University
+    {
+        public string Name { get; set; }
+        public Student[] Students { get; set; }
+    }
+}
