a5da7641dec010ac3ca0d7d32986696aa6e77873
Ivan Maltar <ivan.maltar@fer.hr>
2017-11-02 16:12:10 +0100

    Task 6 ispravljeni, Task7

--- raupjchw2/Task6/Class1.cs
+++ raupjchw2/Task6/Class1.cs
@@ -6,23 +6,27 @@ namespace Task6
 {
     public class Class1
     {
-
-        //async function that caltulates the fractoril of n
         private static async Task<int> FactorialDigitSum(int n)
         {
             if (n <= 0)
             {
                 throw new ArgumentException();
             }
-            int result = 1;
+            int sum = 0;
             await Task.Run(() =>
             {
+                int result = 1;
                 for (int i = 1; i <= n; i++)
                 {
                     result *= i;
                 }
+                while (result > 0)
+                {
+                    sum += result % 10;
+                    result /= 10;
+                }
             });
-            return result;
+            return sum;
         }
     }
 }
---
+++ raupjchw2/Task7/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    public class Class1
+    {
+        private static void LetsSayUserClickedAButtonOnGuiMethod()
+        {
+            var result = GetTheMagicNumberAsync();
+            int[] magicNumbers = result.Result;
+            Console.WriteLine(magicNumbers.Sum());
+        }
+        private static async Task<int[]> GetTheMagicNumberAsync()
+        {
+            return await IKnowIGuyWhoKnowsAGuyAsync();
+        }
+        private static async Task<int[]> IKnowIGuyWhoKnowsAGuyAsync()
+        {
+            var task1 = IKnowWhoKnowsThisAsync(10);
+            var task2 = IKnowWhoKnowsThisAsync(5);
+            
+            return await Task.WhenAll(task1, task2);
+        }
+        private static async Task<int> IKnowWhoKnowsThisAsync(int n)
+        {
+            return await FactorialDigitSum(n);
+        }
+
+        private static async Task<int> FactorialDigitSum(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException();
+            }
+            int sum = 0;
+            await Task.Run(() =>
+            {
+                int result = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    result *= i;
+                }
+                while (result > 0)
+                {
+                    sum += result % 10;
+                    result /= 10;
+                }
+            });
+            return sum;
+        }
+    }
+}
