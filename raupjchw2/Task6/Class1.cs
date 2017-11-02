1a8fed3394e20a71ba8dabc23441eeea56ddc905
Ivan Maltar <ivan.maltar@fer.hr>
2017-11-02 15:16:13 +0100

    Task 6

---
+++ raupjchw2/Task6/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    public class Class1
+    {
+
+        //async function that caltulates the fractoril of n
+        private static async Task<int> FactorialDigitSum(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException();
+            }
+            int result = 1;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    result *= i;
+                }
+            });
+            return result;
+        }
+    }
+}
